9834bff688863b0114eff9a3855e6e54f55cca5a
Nuno Barros <barros@lip.pt>
2023-05-23 19:57:20 +0100

    Added application to probe the existing ports

---
+++ src/probe_ports.cc
@@ -0,0 +1,16 @@
+/*
+ * probe_ports.cc
+ *
+ *  Created on: May 23, 2023
+ *      Author: nbarros
+ */
+
+#include <utilities.hh>
+
+int main(int argc, char**argv)
+{
+  util::enumerate_ports();
+  return 0;
+}
+
+
